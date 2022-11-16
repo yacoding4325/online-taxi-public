effb3957dde38af28114b8149065b80a53b968dd
yxym0203 <1500081802@qq.com>
2022-11-16 21:42:41 +0800

    返回值统一处理代码编写

---
+++ internal-common/src/main/java/com/yacoding/internalcommon/responese/NumberCodeResponse.java
@@ -0,0 +1,14 @@
+package com.yacoding.internalcommon.responese;
+
+import lombok.Data;
+
+/**
+ * @Author yaCoding
+ * @create 2022-11-16 下午 5:23
+ */
+@Data
+public class NumberCodeResponse {
+
+    private int numberCode;
+
+}
