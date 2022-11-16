b9bd7e4f6adae8edebcb0a7daaa12e7333d08687
yxym0203 <1500081802@qq.com>
2022-11-16 17:36:22 +0800

    获取验证码整体流程

--- api-passenger/src/main/java/com/yacoding/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/yacoding/apipassenger/controller/VerificationCodeController.java
@@ -1,6 +1,8 @@
 package com.yacoding.apipassenger.controller;
 
 import com.yacoding.apipassenger.service.VerificationCodeService;
+import com.yacoding.internalcommon.dto.ResponseResult;
+import com.yacoding.internalcommon.request.VerificationCodeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,22 +19,22 @@ public class VerificationCodeController {
     @Autowired
     private VerificationCodeService verificationCodeService;
 
-    @GetMapping("/verification-code")
-    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
-
-        String passengerPhone = verificationCodeDTO.getPassengerPhone();
-        return verificationCodeService.generatorCode(passengerPhone);
-
-    }
-
-    @PostMapping("/verification-code-check")
-    public ResponseResult checkVerificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
-
-        String passengerPhone = verificationCodeDTO.getPassengerPhone();
-        String verificationCode = verificationCodeDTO.getVerificationCode();
-
-        System.out.println("手机号"+passengerPhone+",验证码："+verificationCode);
-
-        return verificationCodeService.checkCode(passengerPhone,verificationCode);
-    }
+//    @GetMapping("/verification-code")
+//    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+//
+//        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+//        return verificationCodeService.generatorCode(passengerPhone);
+//
+//    }
+//
+//    @PostMapping("/verification-code-check")
+//    public ResponseResult checkVerificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+//
+//        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+//        String verificationCode = verificationCodeDTO.getVerificationCode();
+//
+//        System.out.println("手机号"+passengerPhone+",验证码："+verificationCode);
+//
+//        return verificationCodeService.checkCode(passengerPhone,verificationCode);
+//    }
 }
--- api-passenger/src/main/java/com/yacoding/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/yacoding/apipassenger/service/VerificationCodeService.java
@@ -1,5 +1,10 @@
 package com.yacoding.apipassenger.service;
 
+//import com.yacoding.apipassenger.remote.ServiceVefificationcodeClient;
+import com.yacoding.internalcommon.dto.ResponseResult;
+import com.yacoding.internalcommon.request.VerificationCodeDTO;
+import com.yacoding.internalcommon.responese.NumberCodeResponse;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,8 +20,8 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class VerificationCodeService {
 
-    @Autowired
-    private ServiceVefificationcodeClient serviceVefificationcodeClient;
+//    @Autowired
+//    private ServiceVefificationcodeClient serviceVefificationcodeClient;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
@@ -26,23 +31,23 @@ public class VerificationCodeService {
      * @param passengerPhone 手机号
      * @return
      */
-    public ResponseResult generatorCode(String passengerPhone) {
-        // 调用验证码服务，获取验证码
-        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
-        int numberCode = numberCodeResponse.getData().getNumberCode();
+//    public ResponseResult generatorCode(String passengerPhone) {
+//        // 调用验证码服务，获取验证码
+//        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVefificationcodeClient.getNumberCode(6);
+//        int numberCode = numberCodeResponse.getData().getNumberCode();
+//
+//        //存入redis
+//        //key,value，过期时间
+//        String key = RedisPrefixUtils.generatorKeyByPhone(passengerPhone,IdentityConstants.PASSENGER_IDENTITY) ;
+//        //存入redis
+//        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
+//
+//        // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
+//        return ResponseResult.success("");
+//    }
 
-        //存入redis
-        //key,value，过期时间
-        String key = RedisPrefixUtils.generatorKeyByPhone(passengerPhone,IdentityConstants.PASSENGER_IDENTITY) ;
-        //存入redis
-        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
-
-        // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
-        return ResponseResult.success("");
-    }
-
-    @Autowired
-    private ServicePassengerUserClient servicePassengerUserClient;
+//    @Autowired
+//    private ServicePassengerUserClient servicePassengerUserClient;
 
     /**
      * 校验验证码
@@ -50,5 +55,44 @@ public class VerificationCodeService {
      * @param verificationCode 验证码
      * @return
      */
+//    public ResponseResult checkCode(String passengerPhone,String verificationCode) {
+//        //根据手机号，去redis读取
+//        //生成key
+//        String key = RedisPrefixUtils.generatorKeyByPhone(passengerPhone,IdentityConstants.PASSENGER_IDENTITY) ;
+//        //根据key 获取value
+//        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+//        System.out.println("redis中的value："+codeRedis);
+//
+//        //验证码校验
+//        if (StringUtils.isBlank(codeRedis)){
+//            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+//        }
+//
+//        if (!verificationCode.trim().equals(codeRedis.trim())){
+//            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+//        }
+//
+//        // 判断原来是否有用户，并进行对应的处理
+//        VerificationCodeDTO verificationCodeDTO = new VerificationCodeDTO();
+//        verificationCodeDTO.setPassengerPhone(passengerPhone);
+//        servicePassengerUserClient.loginOrRegister(verificationCodeDTO);
+//
+//        // 颁发令牌，不应该用魔法值，用常量
+//        String accessToken = JwtUtils.generatorToken(passengerPhone, IdentityConstants.PASSENGER_IDENTITY, TokenConstants.ACCESS_TOKEN_TYPE);
+//        String refreshToken = JwtUtils.generatorToken(passengerPhone, IdentityConstants.PASSENGER_IDENTITY ,TokenConstants.REFRESH_TOKEN_TYPE);
+//
+//        // 将token存到redis当中
+//        String accessTokenKey = RedisPrefixUtils.generatorTokenKey(passengerPhone , IdentityConstants.PASSENGER_IDENTITY , TokenConstants.ACCESS_TOKEN_TYPE);
+//        stringRedisTemplate.opsForValue().set(accessTokenKey , accessToken , 30, TimeUnit.DAYS);
+//
+//        String refreshTokenKey = RedisPrefixUtils.generatorTokenKey(passengerPhone , IdentityConstants.PASSENGER_IDENTITY , TokenConstants.REFRESH_TOKEN_TYPE);
+//        stringRedisTemplate.opsForValue().set(refreshTokenKey , refreshToken , 31, TimeUnit.DAYS);
+//
+//        // 响应
+//        TokenResponse tokenResponse = new TokenResponse();
+//        tokenResponse.setAccessToken(accessToken);
+//        tokenResponse.setRefreshToken(refreshToken);
+//        return ResponseResult.success(tokenResponse);
+//    }
 
 }
