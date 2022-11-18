1607c2026a0a3033c2c388f42532587df9606c1b
yxym0203 <1500081802@qq.com>
2022-11-18 17:22:53 +0800

    jwt拦截器的编写

---
+++ api-passenger/src/main/java/com/yacoding/apipassenger/interceptor/InterceptorConfig.java
@@ -0,0 +1,33 @@
+package com.yacoding.apipassenger.interceptor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author yaCoding
+ * @create 2022-11-18 下午 2:48
+ */
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public JwtInterceptor jwtInterceptor() {
+        return new JwtInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor())
+                //拦截的路径
+                .addPathPatterns("/**")
+                // 不拦截的路径
+                .excludePathPatterns("/noauthTest")
+                .excludePathPatterns("/verification-code")
+                .excludePathPatterns("/verification-code-check")
+                .excludePathPatterns("/token-refresh")
+                .excludePathPatterns("/test-real-time-order/**");
+    }
+}
