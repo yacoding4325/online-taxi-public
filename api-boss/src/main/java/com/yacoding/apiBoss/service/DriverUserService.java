ef3d07af1d84d1555ab3ad03f8837103b36f6ddb
yxym0203 <1500081802@qq.com>
2022-12-01 16:19:20 +0800

    api-boss bug的修改

--- api-boss/src/main/java/com/yacoding/apiBoss/service/DriverUserService.java
+++ api-boss/src/main/java/com/yacoding/apiBoss/service/DriverUserService.java
@@ -3,7 +3,6 @@ package com.yacoding.apiBoss.service;
 import com.yacoding.apiBoss.remote.ServiceDriverUserClient;
 import com.yacoding.internalcommon.dto.DriverUser;
 import com.yacoding.internalcommon.dto.ResponseResult;
-import com.yacoding.serviceDriverUser.mapper.DriverUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
