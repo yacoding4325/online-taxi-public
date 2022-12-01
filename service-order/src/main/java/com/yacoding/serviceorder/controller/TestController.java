cc658e406521271d796e04cad0206832b7f4e6d5
yxym0203 <1500081802@qq.com>
2022-12-01 15:59:45 +0800

    测试派单逻辑

--- service-order/src/main/java/com/yacoding/serviceorder/controller/TestController.java
+++ service-order/src/main/java/com/yacoding/serviceorder/controller/TestController.java
@@ -1,6 +1,12 @@
 package com.yacoding.serviceorder.controller;
 
+import com.yacoding.internalcommon.dto.OrderInfo;
+import com.yacoding.serviceorder.mapper.OrderInfoMapper;
+import com.yacoding.serviceorder.service.OrderInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +19,28 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         return "service-order test";
+    }
+
+    @Autowired
+    OrderInfoService orderInfoService;
+
+    @Autowired
+    OrderInfoMapper orderInfoMapper;
+
+    @Value("${server.port}")
+    String port;
+
+    /**
+     * 测试派单逻辑
+     * @param orderId
+     * @return
+     */
+    @GetMapping("/test-real-time-order/{orderId}")
+    public String dispatchRealTimeOrder(@PathVariable("orderId") long orderId){
+        System.out.println("service-order 端口："+ port+" 并发测试：orderId："+orderId);
+        OrderInfo orderInfo = orderInfoMapper.selectById(orderId);
+        orderInfoService.dispatchRealTimeOrder(orderInfo);
+        return "test-real-time-order   success";
     }
 
 }
