381188dedb797157c53bef4d0cf67fb3f76e6e50
yxym0203 <1500081802@qq.com>
2022-12-02 16:05:31 +0800

    并发测试-单机/集群

--- api-passenger/src/main/java/com/yacoding/apipassenger/remote/ServiceOrderClient.java
+++ api-passenger/src/main/java/com/yacoding/apipassenger/remote/ServiceOrderClient.java
@@ -3,6 +3,7 @@ package com.yacoding.apipassenger.remote;
 import com.yacoding.internalcommon.dto.ResponseResult;
 import com.yacoding.internalcommon.request.OrderRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,5 +17,8 @@ public interface ServiceOrderClient {
 
     @RequestMapping(method = RequestMethod.POST,value = "order/add" )
     public ResponseResult add(@RequestBody  OrderRequest orderRequest);
+
+    @RequestMapping(method = RequestMethod.GET,value = "/test-real-time-order/{orderId}")
+    public String dispatchRealTimeOrder(@PathVariable("orderId") long orderId);
 
 }
