e71c19e5bcd9b1e442f22f38d5f0a8231792ca4f
yxym0203 <1500081802@qq.com>
2022-12-05 22:48:14 +0800

    轨迹查询

--- serice-map/src/main/java/com/yacoding/servicemap/service/TerminalService.java
+++ serice-map/src/main/java/com/yacoding/servicemap/service/TerminalService.java
@@ -28,6 +28,13 @@ public class TerminalService {
         return terminalClient.aroundsearch(center,radius);
     }
 
+    /**
+     * 轨迹查询
+     * @param tid
+     * @param starttime
+     * @param endtime
+     * @return
+     */
     public ResponseResult<TrsearchResponse> trsearch(String tid , Long starttime , Long endtime){
 
         return terminalClient.trsearch(tid,starttime,endtime);
